bd2a8a8c01ded7b3a859bfc2a7f3a170f86900de
capn <kehsa754@gmail.com>
2025-04-11 17:58:10 +0000

    render

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -8,12 +8,10 @@ load_dotenv()
 
 
 def get_connection():
-    # Проверяем, что DATABASE_URL загружен
     database_url = os.getenv("DATABASE_URL")
     if not database_url:
         raise ValueError("DATABASE_URL не найден в переменных окружения!")
     
-    # Подключаемся с SSL (обязательно для Render)
     return psycopg2.connect(database_url, sslmode="require")
 
 
